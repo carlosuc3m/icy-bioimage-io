b93b1dfc68741a09ac1012ad0cbd39cd36e79f40
carlosuc3m <100329787@alumnos.uc3m.es>
2023-12-20 13:34:02 +0100

    keep working on the ci tests

--- src/main/java/io/bioimage/modelrunner/ci/ContinuousIntegration.java
+++ src/main/java/io/bioimage/modelrunner/ci/ContinuousIntegration.java
@@ -19,13 +19,24 @@
  */
 package io.bioimage.modelrunner.ci;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.jar.Manifest;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import io.bioimage.modelrunner.tensor.Tensor;
+import io.bioimage.modelrunner.utils.Constants;
+import io.bioimage.modelrunner.utils.YAMLUtils;
 
 /**
  * 
@@ -52,12 +63,47 @@ public class ContinuousIntegration {
     }
 
 	
-	public static void main2(String[] args) {
-		if (args.length == 0) {
-			args = new String[] {};
-		}
+	public static void runTests(Path rdfDir, String resourceID, String versionID, Path summariesDir, String postfix) {
+		HashMap<String, String> summaryDefaults = new HashMap<String, String>();
+		postFix = getJDLLVersion();
+		summaryDefaults.put("JDLL_VERSION", postfix);
 		
+		PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + resourceID + File.separator + versionID + File.separator + Constants.RDF_FNAME);
+
+        List<Path> rdfFiles = Files.walk(rdfDir).collect(Collectors.toList());
+		
+		for (Path rdfPath : rdfFiles) {
+			String testName = "Reproduce ouptuts with JDLL " + postfix;
+			String error = null;
+			String status = null;
+			
+			Map<String, Object> rdf = new HashMap<String, Object>();
+			try {
+				rdf = YAMLUtils.load(rdfPath.toString());
+			} catch (Exception ex) {
+				error = "Unable to load " + Constants.RDF_FNAME + ": " + ex.toString();
+				status = "failed";
+			}
+		}
+
 		
 		
 	}
+	
+	/**
+	 * Code to automatically get the version of JDLL being used
+	 * @return the JDLL version being used
+	 */
+	public static String getJDLLVersion() {
+		String version = "UNKNOWN";
+        try (InputStream manifestStream = Tensor.class.getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF")) {
+            if (manifestStream != null) {
+                Manifest manifest = new Manifest(manifestStream);
+                java.util.jar.Attributes attrs = manifest.getMainAttributes();
+                version = attrs.getValue("Implementation-Version");
+            }
+        } catch (Exception e) {
+        }
+        return version;
+    }
 }
